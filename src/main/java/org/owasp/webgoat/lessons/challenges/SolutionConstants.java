fa53ee581530d2888106285f505235d961826155
Dylan-ASIR <albertoph4@gmail.com>
2026-06-07 14:18:31 +0200

    Update SolutionConstants.java

--- src/main/java/org/owasp/webgoat/lessons/challenges/SolutionConstants.java
+++ src/main/java/org/owasp/webgoat/lessons/challenges/SolutionConstants.java
@@ -7,5 +7,5 @@ package org.owasp.webgoat.lessons.challenges;
 public interface SolutionConstants {
 
   // TODO should be random generated when starting the server
-  String PASSWORD = "!!webgoat_admin_1234!!";
+  String PASSWORD = java.util.UUID.randomUUID().toString();
 }
